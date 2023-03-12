5afa2e7ecc31f81f795c02d9838eb8defa252429
renkayla <116777951+renkayla@users.noreply.github.com>
2023-03-11 21:19:42 -0500

    declared variables in js

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,17 @@
 // Assignment code here
 
-   // Declaring the Variable \\
- 
-                    
-                    // Calling the Array \\
-                    // Console.log to see the change!
+// arrays of all necessary string data types
+var upperCase = ["A", "B", "C", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowerCase = ["a", "b", "c", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"] 
+var number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", ";", ":", "'", "/", "?", ".", ">", ",", "<", "`", "~","|",]
 
-                    
-                    // length() Method \\
-                    // Console.log to see the change!
-
-                    
-                    // indexOf() Method \\
-                    // Reminder! Arrays start at 0! \\
-                    
-                    
-                    // reverse() Method \\
-                    // Console.log to see the change!
-
-
-                    // sort() Method \\
-                    // Console.log to see the change!
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -36,3 +24,16 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// Declaring  Variables
+var confirmUpperCase;
+var confirmLowerCase;
+var confirmNumber;
+var confirmSpecialChar;
+var confirmLength = "";
+
+// confirming length of characters with prompt
+function generatePassword() {
+  var confirmUpperCase;
+}
